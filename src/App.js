12a273b330916c77a90dd0a7948f9d0c05692426
Shaun Thornburgh <personal@shaunthornburgh.com>
2024-08-14 10:49:41 +0100

    Books component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BooksProvider } from "./BooksContext";
-
+import Books from "./components/Books";
 function App() {
   return (
     <BooksProvider>
-      <div className="App"></div>
+      <Books></Books>
     </BooksProvider>
   );
 }
---
+++ src/components/Books.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { BooksContext } from "../BooksContext";
+
+export default function Books() {
+  const books = useContext(BooksContext);
+
+  return (
+    <div>
+      <h1>All books</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            {book.title} by {book.author}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
