9b71a595a2f713245e072b20033a50fe120621ab
Shaun Thornburgh <personal@shaunthornburgh.com>
2024-08-22 10:24:54 +0100

    Add search by query params

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { BooksContext } from "../BooksContext";
 
 export default function Books() {
+  const query = new URLSearchParams(useLocation().search);
+  const search = query.get("search") || "";
   const books = useContext(BooksContext);
+
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div>
       <h1>All books</h1>
       <ul>
-        {books.map((book) => (
+        {filteredBooks.map((book) => (
           <li key={book.id}>
             {book.title} by {book.author}
           </li>
